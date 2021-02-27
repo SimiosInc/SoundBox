2ede200f1eadbb8a3f61d0213967b3a5ab2c428a
Desik <jahdesik@gmail.com>
2021-02-27 17:53:42 -0600

    updated _sounds.bat

---
+++ _sounds.bat
@@ -0,0 +1,16 @@
+@echo off
+echo This script will now prepare the files for using SoundBox
+
+echo    SOUNDS TABLE
+echo local _, sounds = ... >> ..\SoundBox\sounds.lua
+echo.>> ..\SoundBox\sounds.lua
+echo -- --------------- >> ..\SoundBox\sounds.lua
+echo --   SOUNDS TABLE >> ..\SoundBox\sounds.lua
+echo -- --------------- >> ..\SoundBox\sounds.lua
+echo.>> ..\SoundBox\sounds.lua
+for %%F in (..\SoundBox\sound\*.*) do (
+echo       %%~nF
+echo    sounds["%%~nF"] = "%%~nF" >> ..\SoundBox\sounds.lua
+)
+:end_of_file
+pause
