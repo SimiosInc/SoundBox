4a6a00e5e073f340037bfb7e3249a83f0fcf3b2a
Desik <jahdesik@gmail.com>
2021-03-01 01:31:47 -0600

    -Moved Sounds table to own file - Added Debug fuction - Added who command - Added help fuction - Added DND mode

--- main.lua
+++ main.lua
@@ -1,98 +1,125 @@
+local _, S = ...
+
 local prefix = "SoundBox"
 local songPath = "Interface\\AddOns\\SoundBox\\sound\\"
 local playerName = UnitName("player")
+local soundHandle
+local soundFileID = 0
+local playList = {}
+local sounds = S
+local setWho = true
+local setDND = false
+local setDebug = false
+local version
 
--- -----
---   SOUNDS TABLE
--- -----
-local sounds = {
-    aiuda = "aiuda", 
-    allahu = "allahu", 
-    babyshark = "babyshark", 
-    badum = "badum", 
-    bruh = "bruh", 
-    bryant = "bryant", 
-    cabra = "cabra", 
-    careless = "careless", 
-    cato = "cato", 
-    combo = "combo", 
-    delfin = "delfin", 
-    digievo = "digievo", 
-    dolly = "dolly", 
-    dungadunga = "dungadunga", 
-    duri = "duri", 
-    esecompa = "ese_compa", 
-    finishhim = "finish_him", 
-    gura = "gawr_gura_a", 
-    goh = "get_over_here", 
-    giorno = "giorno", 
-    gurgura = "gurgura", 
-    kala = "kala", 
-    krillin = "krillin", 
-    kya = "kya", 
-    maduro = "maduro_chupalo", 
-    mvc = "marvel_versus", 
-    mclimax = "medusa_climax", 
-    mintro = "medusa_intro", 
-    migatte = "migatte", 
-    migattefail = "migatte_fail", 
-    missioncomplete = "missioncomplete", 
-    modem = "modem", 
-    nani = "nani", 
-    niconi = "niconi", 
-    nos = "nos_estan_atacando", 
-    pillarmen = "pillarmen", 
-    plusultra = "plusultra", 
-    powaa = "powaa", 
-    rezero = "rezero", 
-    rosa = "rosa", 
-    turtle = "turtle", 
-    tuturu = "tuturu", 
-    uoh = "uoh", 
-    timmy = "vamos_timmy", 
-    vcf = "vcf", 
-    vegeta = "vegeta", 
-    windows = "windows", 
-    wow = "wow", 
-    yametekudasai = "yametekudasai", 
-    yare = "yare", 
-    yubarta = "yubarta", 
-    zawarudo = "zawarudo" 
- }
+--   Create listener Frame
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("CHAT_MSG_ADDON")
 f:SetScript(
     "OnEvent",
     function(self, event, prefix, message, channel, sender)
-        if prefix ~= "SoundBox" then
-            return
+        if event == "CHAT_MSG_ADDON" and prefix then
+
+            local fileName = sounds[message]
+            if not fileName then
+                return
+            end
+
+            if not setDND then
+                local soundFile = songPath .. fileName .. ".ogg"
+                local ok, _, handle = pcall(PlaySoundFile, soundFile, "Master")
+
+                if ok then
+                    soundHandle = handle
+                    soundFileID = soundFileID + 1
+                    playList[soundFileID] = handle
+                end
+
+                if setWho == true and ok == true then
+                    local name = Ambiguate(sender, "short")
+                    print("|cff00FF96SoundBox|r: |cFFFF8040" .. name .. "|r |cFFFF8040played " .. message .. "|r")
+                end
+                --@debug@
+                if setDebug then
+                    print(
+                        "|cff00FF96Prefix|r: " .. prefix .. "\n",
+                        "|cff00FF96Path|r: " .. songPath .. "\n",
+                        "|cff00FF96File|r: " .. fileName .. "\n",
+                        "|cff00FF96By|r: " .. sender .. "\n",
+                        "|cff00FF96In|r: " .. channel .. "\n",
+                        "|cff00FF96Handle|r: " .. handle .. "\n"
+                    )
+                end
+                --@end-debug@
+            end
         end
-
-        local soundFile = sounds[message]
-        if not soundFile then
-            return
-        end
-
-        PlaySoundFile(songPath .. soundFile .. ".ogg", "Master")
-
-         --[[ 
-        print("\n"..
-            "|cff00FF96Prefix|r:" .. prefix.. "\n",
-            "|cff00FF96Path|r:" .. songPath.. "\n" ,
-            "|cff00FF96File|r:" .. soundFile.. "\n" ,
-            "|cff00FF96By|r:" .. sender.. "\n" ,
-            "|cff00FF96In|r:" .. channel.. "\n" 
-        ) 
-         --]]
     end
 )
 
+-- Create Sender Slash Command
+
 SLASH_SB1 = "/sb"
 SlashCmdList.SB = function(message)
-    -- Find the right channel for your current group type:
-    local channel
+    if message == "stop" then
+        for _, handle in pairs(playList) do
+            StopSound(handle)
+            playList = {}
+            --@debug@
+            if setDebug then
+                print("|cff00FF96Stopped|r: " .. soundHandle)
+            end
+            --@end-debug@
+        end
+    end
 
+    if message == "version" then
+        version = GetAddOnMetadata("SoundBox", "version")
+        print("|cff00FF96SoundBox|r: |cFFFF8040Version: |r" .. version)
+    end
+
+    if message == "help" then
+        local s = ""
+        sounds.sounds = "cmd"
+        for k, _ in pairs(sounds) do
+            s = s .. k .. ", "
+        end
+        print("|cff00FF96SoundBox|r\124|Help: |cFFFF8040/sb " .. s .. "|r")
+    end
+
+    if message == "dnd" then
+        if setDND then
+            setDND = false
+            print("|cff00FF96SoundBox|r\124|Set:|cFFFF8040 Do Not Disturb OFF|r")
+        else
+            setDND = true
+            print("|cff00FF96SoundBox|r\124|Set:|cFFFF8040 Do Not Disturb ON|r")
+        end
+    end
+    
+    --@debug@
+    if message == "debug" then
+        if setDebug then
+            setDebug = false
+            print("|cff00FF96SoundBox|r\124|Debug:|cFFFF8040 OFF|r")
+        else
+            setDebug = true
+            print("|cff00FF96SoundBox|r\124|Debug:|cFFFF8040 ON|r")
+        end
+    end
+    --@end-debug@
+
+    if message == "who" then
+        if setWho then
+            setWho = false
+            print("|cff00FF96SoundBox|r\124|Set:|cFFFF8040 Announcer OFF|r")
+        else
+            setWho = true
+            print("|cff00FF96SoundBox|r\124|Set:|cFFFF8040 Announcer ON|r")
+        end
+    end
+
+    local channel
     if IsInGroup() then
         if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
             channel = "INSTANCE_CHAT"
@@ -104,7 +131,8 @@ SlashCmdList.SB = function(message)
     else
         C_ChatInfo.SendAddonMessage(prefix, message, "WHISPER", playerName)
     end
-    -- convert to lowercase so it will match the table keys:
+
+    -- convert to lowercase
     message = strlower(strtrim(message))
 
     -- Send it:
