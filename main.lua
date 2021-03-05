42a509a103974b4bcea0ce3da13326ff49b49361
Desik <jahdesik@gmail.com>
2021-03-04 21:57:56 -0600

    Added comments so is even easier to understand Moved stuff around so it doesnt look bad Look like we are done here for now

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 local _, S = ...
 
 local addonPrefix = "SoundBox"
+local version = GetAddOnMetadata("SoundBox", "version")
 local folderName = "Interface\\AddOns\\SoundBox\\sound\\"
 local playerName = UnitName("player")
 local soundHandle
@@ -10,31 +11,43 @@ local sounds = S
 local setWho = true
 local setDND = false
 local setDebug = false
-local version
 
--- Globals
+-- Register addon prefix
 
-_G ["BINDING_HEADER_SoundBox"] = "|cff00FF96SoundBox|r"
-_G ["BINDING_HEADER_SOUNDBOX_KEYBIND_ALLSOUNDS"] = "All Sounds"
-_G ["BINDING_NAME_SOUNDBOX_STOP_SOUND"] = "Stop Sound"
+C_ChatInfo.RegisterAddonMessagePrefix(addonPrefix)
 
---   Create listener Frame
+-- Register Keybinding globals
+
+_G["BINDING_HEADER_SoundBox"] = "|cff00FF96SoundBox|r"
+_G["BINDING_HEADER_SOUNDBOX_KEYBIND_ALLSOUNDS"] = "All Sounds"
+_G["BINDING_NAME_SOUNDBOX_STOP_SOUND"] = "Stop Sound"
+
+-- Create listener Frame
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("CHAT_MSG_ADDON")
 f:SetScript(
     "OnEvent",
     function(self, event, prefix, message, channel, sender)
+        if event == "CHAT_MSG_ADDON" and prefix == addonPrefix then
 
-        if event == "CHAT_MSG_ADDON" and prefix == addonPrefix then            
+            -- compare the message with our sounds table
+
             local fileName = sounds[message]
             if not fileName then
                 return
             end
 
+            -- we check if DND is on, proceed otherwise
+
             if not setDND then
+
+                -- we compose sound file path
+
                 local soundPath = folderName .. fileName .. ".ogg"
                 local ok, _, handle = pcall(PlaySoundFile, soundPath, "Master")
+
+                -- Once we have the soundpath, we get the handleID too
 
                 if ok then
                     soundHandle = handle
@@ -42,10 +55,13 @@ f:SetScript(
                     playList[soundFileID] = handle
                 end
 
+                -- we check if the announcer is on and that we actually have a valid message to print
+
                 if setWho == true and ok == true then
                     local name = Ambiguate(sender, "short")
                     print("|cff00FF96SoundBox|r: |cFFFF8040" .. name .. "|r |cFFFF8040played " .. message .. "|r")
                 end
+
                 --@debug@
                 if setDebug then
                     print(
@@ -54,7 +70,7 @@ f:SetScript(
                         "|cff00FF96File|r: " .. fileName .. "\n",
                         "|cff00FF96By|r: " .. sender .. "\n",
                         "|cff00FF96In|r: " .. channel .. "\n",
-                        "|cff00FF96Handle|r: " .. handle .. "\n"
+                        "|cff00FF96Handle|r: " .. handle
                     )
                 end
                 --@end-debug@
@@ -63,29 +79,29 @@ f:SetScript(
     end
 )
 
+-- function to stop all sounds
+
 function SoundBox_Stop()
-        for _, handle in pairs(playList) do
-            StopSound(handle)
-            playList = {}
-            --@debug@
-            if setDebug then
-                print("|cff00FF96Stopped|r: " .. soundHandle)
-            end
-            --@end-debug@
+    for _, handle in pairs(playList) do
+        StopSound(handle)
+        playList = {}
+        --@debug@
+        if setDebug then
+            print("|cff00FF96Stopped|r: " .. soundHandle)
         end
+        --@end-debug@
+    end
 end
 
 -- Create Sender Slash Command
 
 SLASH_SB1 = "/sb"
 SlashCmdList.SB = function(message)
-
     if message == "stop" then
-       SoundBox_Stop()          
+        SoundBox_Stop()
     end
 
     if message == "version" then
-        version = GetAddOnMetadata("SoundBox", "version")
         print("|cff00FF96SoundBox|r: |cFFFF8040Version: |r" .. version)
     end
 
@@ -99,12 +115,12 @@ SlashCmdList.SB = function(message)
 
     if message == "help" or message == "?" or message == "" then
         print(
-        "|cff00FF96SoundBox|r\124|Help: type |cff00FF96/sb \"yoursound\"|r to meme\n",
-        "|cff00FF96/sb stop|r to stop all current sounds playing or set up a keybind!\n",
-        "|cff00FF96/sb sounds|r will display all sounds available\n",
-        "|cff00FF96/sb dnd|r to enable or disable \"Do Not Disturb\" mode\n",
-        "|cff00FF96/sb who|r to enable or disable the announcer\n",
-        "|cff00FF96/sb version|r to see what version you are running"
+            '|cff00FF96SoundBox|r\124|Help: You might want to type |cff00FF96/sb "sound"|r to send sounds or\n',
+            "|cff00FF96/sb stop|r to stop all current sounds playing or set up a keybind!\n",
+            "|cff00FF96/sb sounds|r will display all sounds available\n",
+            '|cff00FF96/sb dnd|r to enable or disable "Do Not Disturb" mode\n',
+            "|cff00FF96/sb who|r to enable or disable the announcer\n",
+            "|cff00FF96/sb version|r to see what version you are running"
         )
     end
 
@@ -140,6 +156,8 @@ SlashCmdList.SB = function(message)
         end
     end
 
+    -- Set up the channel depending of group or solo
+
     local channel
     if IsInGroup() then
         if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
@@ -150,18 +168,17 @@ SlashCmdList.SB = function(message)
             channel = "PARTY"
         end
     else
-
         C_ChatInfo.SendAddonMessage(addonPrefix, message, "WHISPER", playerName)
-
     end
 
-    -- convert to lowercase
+    -- convert message to lowercase
+
     message = strlower(strtrim(message))
 
-    -- check if DND is on and send the message if not:
-    if not setDND then
-    C_ChatInfo.SendAddonMessage(addonPrefix, message, channel)
-    end
-end
+    -- check if DND is on, send the message otherwise
 
-C_ChatInfo.RegisterAddonMessagePrefix(addonPrefix)
+    if not setDND then
+        C_ChatInfo.SendAddonMessage(addonPrefix, message, channel)
+    end
+
+end
